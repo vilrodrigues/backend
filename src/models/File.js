1a3111c97bc43cff73943f903ff6598c8be41bf7
Vilson Rodrigues Junior <vilrodriguesjr@gmail.com>
2019-04-10 10:24:11 -0300

    Update File.js

--- src/models/File.js
+++ src/models/File.js
@@ -21,7 +21,8 @@ const File = new mongoose.Schema(
 File.virtual("url").get(function(){
         const url = process.env.URL || "http://localhost:3333"
         
-        return `http://localhost:3333/files/${encodeURIComponent(this.path)}`;
+        return ${url}/files/${encodeURIComponent(this.path)};
+        //return `http://localhost:3333/files/${encodeURIComponent(this.path)}`;
 })
 
 module.exports = mongoose.model("File", File);
